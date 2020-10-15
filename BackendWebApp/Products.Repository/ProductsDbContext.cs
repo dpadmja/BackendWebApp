102e82ad86825710a15a99d1fce8c5d7c6447aad
Padmaja Deshpande <padmaja.deshpande@autodesk.com>
2020-10-15 11:18:38 +0530

    Added Products controller with CRUD operations

---
+++ BackendWebApp/Products.Repository/ProductsDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Products.Repository.RepoModels;
+
+//using Products.Repository.RepoModels;
+
+namespace Products.Repository
+{
+    public class ProductsDbContext : DbContext
+    {
+        public ProductsDbContext(DbContextOptions<ProductsDbContext> dbOptions) : base(dbOptions)
+        {
+
+        }
+
+        public ProductsDbContext()
+        {
+
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
